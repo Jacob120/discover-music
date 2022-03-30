46ae2d3375487584c0d6f0ded5dfe5d53f7154bc
Jacob Kozik <jakubkozik120@gmail.com>
2022-03-30 15:39:18 +0200

    Add TO DO Green audio player plugin

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -25,55 +25,18 @@ class Home {
     const homeContainer = document.querySelector(select.containerOf.home);
     /* add element to menu */
     
-    homeContainer.appendChild(thisHome.element);
-    
-    
-    // thisHome.dom = {
-    //   wrapper: element,
-    // };
-
-    // thisHome.pages = document.querySelector(select.containerOf.pages).children;
-    // thisHome.navBar = document.querySelectorAll(select.nav.links);
-
-    // const idFromHash = window.location.hash.replace('#/', '');
-
-    // let pageMatchingHash = thisHome.pages[0].id;
-
-    // for(let page of thisHome.pages){
-    //   if(page.id == idFromHash){
-    //     pageMatchingHash = page.id;
-    //     console.log(pageMatchingHash);
-    //     break;
-    //   } 
-    // }
-
-    // thisHome.activatePage(pageMatchingHash);
-    // for(let link of thisHome.navLinks){
-    //   link.addEventListener('click', function(event){
-    //     const clickedElement = this;
-    //     event.preventDefault();
-
-    //     /*  get page id from href attribute*/
-
-    //     const id = clickedElement.getAttribute('href').replace('#', '');
-
-    //     /* run thisApp.activatePage with that id */
-
-    //     thisHome.activatePage(id);
-
-    //     /* change URL hash*/
-
-    //     window.location.hash = '#/' + id;
-
-    //   });
-    // }
+    homeContainer.appendChild(thisHome.element);  
   }
 
   /* eslint-disable */ 
   initWidgets() {    
-    const gap = new GreenAudioPlayer('.gap-example');
+    setTimeout( function() {
+      GreenAudioPlayer.init({
+        selector: '.player', // inits Green Audio Player on each audio container that has class "player"
+        stopOthersOnPlay: true
+    });
+    }, 2000)
   }
-
 }
 
 export default Home;
