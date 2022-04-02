1607ca10b61aeeece02be12ab5f2c3df91fc0875
Jakub Kozik <jakubkozik120@gmail.com>
2022-04-02 14:43:08 +0200

    Fix initSearch

--- src/js/components/Search.js
+++ src/js/components/Search.js
@@ -4,13 +4,14 @@ import utils from '../utils.js';
 class Search {
   constructor(id, data){
     const thisSearch = this;
-
+    
     thisSearch.id = id;
     thisSearch.data = data;
 
     thisSearch.render();
     thisSearch.getElements();
     thisSearch.initSearch();
+
   }
 
   render(){
@@ -27,68 +28,31 @@ class Search {
     searchContainer.appendChild(thisSearch.element);    
   }  
 
-  // getElements(element) {
-  //   const thisSearch = this;
-
-  //   thisSearch.dom = {
-  //     wrapper: element,
-  //     gapPlayer: element.querySelector(select.player.wrapper),
-  //     authorAndTitle: element.querySelector(select.player.author_title),
-  //     songDescription: element.querySelector(select.player.description),  //     
-  //   };
-
- 
-    
-  // }
-
   getElements(){
     const thisSearch = this;
 
-    thisSearch.gapPlayer = thisSearch.element.querySelector(select.player.wrapper);
-    thisSearch.authorAndTitle = thisSearch.element.querySelector(select.player.author_title);
-    thisSearch.songDescription = thisSearch.element.querySelector(select.player.description);
-    thisSearch.searchInput  = document.querySelector(select.search.input);
-    thisSearch.searchList = document.querySelectorAll('.search-li');   
-    
-
-    // console.log('gapPlayer', thisSearch.gapPlayer);
-    console.log('authorandtitle', thisSearch.authorAndTitle.textContent);
-    // console.log('songDescription', thisSearch.songDescription);
-    console.log('searchList', thisSearch.searchList);
-
+    thisSearch.playerWrapper = document.querySelectorAll(select.player.wrapper); 
+    thisSearch.searchInput  = document.querySelector(select.search.input);  
   }
 
   initSearch(){
-    const thisSearch = this;
+    const thisSearch = this;  
 
-
-      
+    const arr = Array.from(thisSearch.playerWrapper);  
+    
     thisSearch.searchInput.addEventListener('input', function(e){
-      const value = e.target.value.toLowerCase();
-      console.log(value);
-
-      // for(const listElement of thisSearch.searchList){
-      //   let list = listElement.textContent.toLowerCase();
-
-      //   if(list.includes(value)){
-      //     listElement.style.display = 'block';
-      //   } else {
-      //     listElement.style.display = 'none';
-      //   }
-      // }
-
-      thisSearch.searchList.forEach(list => {
-
-        const isVisible = thisSearch.authorAndTitle.textContent.toLowerCase().includes(value) || 
-                          thisSearch.songDescription.textContent.toLowerCase().includes(value);
-        // console.log('player', player);
-        list.classList.toggle(classNames.elements.hidden, !isVisible);
-        console.log('list', list);
-      });
+      const value = e.target.value.toLowerCase(); 
       
- 
-    });
+      arr.forEach((domElement) => {        
+   
+        const isVisible = domElement.textContent.toLowerCase().includes(value) ||
+                        domElement.textContent.toLowerCase().includes(value);  
+
+        domElement.classList.toggle(classNames.elements.hidden, !isVisible);  
+      });
+    }); 
   }
+ 
 }
 
 export default Search;
